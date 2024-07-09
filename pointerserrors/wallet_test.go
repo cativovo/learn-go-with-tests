493bd9892e52aedc5fc7cf21af5495b6a344b53f
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-07-09 17:32:18 +0800

    some updates

---
+++ pointerserrors/wallet_test.go
@@ -0,0 +1,18 @@
+package pointerserrors
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Fatalf("got %s, want %s", got, want)
+	}
+}
